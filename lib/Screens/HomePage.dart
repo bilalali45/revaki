8e95b598861089c9caed82fbb4c0d0ada147397b
syedbilalali <bilal.ali@technado.co>
2022-02-12 03:32:54 +0500

    update

--- lib/Screens/HomePage.dart
+++ lib/Screens/HomePage.dart
@@ -1,8 +1,9 @@
-
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:revaki/constants/assests_image.dart';
+import 'package:revaki/widgets/PopularFoodsWidget.dart';
+import 'package:revaki/widgets/SearchWidget.dart';
+import 'package:revaki/widgets/TopMenus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -10,35 +11,84 @@ class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
+final GlobalKey<ScaffoldState> _key = GlobalKey(); // Create a key
+
 var contxt;
 
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     contxt = context;
+
     final height = MediaQuery.of(context).size.height;
     return WillPopScope(
         onWillPop: () => Future.value(false),
-       child: Container(
-        decoration:new BoxDecoration(
-            image:  new DecorationImage(
+        child: Container(
+            decoration: new BoxDecoration(
+                image: new DecorationImage(
               image: new AssetImage(background),
-              fit: BoxFit.cover,)
+              fit: BoxFit.cover,
+            )),
+            child: Scaffold(
+                key: _key,
+                backgroundColor: Colors.transparent,
+                drawer: Drawer(
+                  child: ListView(
+                    padding: EdgeInsets.zero,
+                    children: const <Widget>[
+                      DrawerHeader(
+                        decoration: BoxDecoration(
+                          color: Colors.blue,
+                        ),
+                        child: Text(
+                          'Drawer Header',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 24,
+                          ),
+                        ),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.message),
+                        title: Text('Messages'),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.account_circle),
+                        title: Text('Profile'),
+                      ),
+                      ListTile(
+                        leading: Icon(Icons.settings),
+                        title: Text('Settings'),
+                      ),
+
+                    ],
+
+                  ),
+
+                ),
+                appBar: new AppBar(
+                   title: Text("Home"),
+                  leading: new IconButton(
+                  icon: new Icon(Icons.menu),
+                  onPressed: () => _key.currentState!.openDrawer(),
         ),
-        child : Scaffold(
-            backgroundColor: Colors.transparent,
-            body: Container(
-                height: height,
-                child: Stack(
-                    children: [
+    ),
+              body: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    //SearchWidget(),
+                     TopMenus(),
+                      PopularFoodsWidget(),
+                   // BestFoodWidget(),
+                  ],
+                ),
+              ),
+
+            ),
 
 
-                    ]
-                )
-            )
         )
-      )
+
     );
   }
-
 }
